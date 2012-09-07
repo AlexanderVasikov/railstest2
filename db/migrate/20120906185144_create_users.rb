62daffc22c975e20e8c190b629f24f955d81902a
kaktus <kaktusss02@gmail.com>
2012-09-07 12:42:09 +0400

    Initial commit

--- db/migrate/20120906185144_create_users.rb
+++ db/migrate/20120906185144_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table "users" do |t|
-      #t.column :login,                     :string, :limit => 40
+      t.column :login,                     :string, :limit => 40
       t.column :name,                      :string, :limit => 100, :default => '', :null => true
       t.column :email,                     :string, :limit => 100
       t.column :crypted_password,          :string, :limit => 40
